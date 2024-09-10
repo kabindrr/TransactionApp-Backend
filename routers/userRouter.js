5fb81e4eeb39a593974baa897e9eaa9a1a156af4
kabindrr <chaina_kabindra@yahoo.com>
2024-09-10 11:28:38 +1000

    hash pass

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,11 +1,16 @@
 import express from "express";
 import { addUser } from "../models/UserModal.js";
+import { hashPassword } from "../utils/bcrypt.js";
 
 const router = express.Router();
 
 // user signup
 router.post("/", async (req, res) => {
   try {
+    // encrypt the password
+    req.body.password = hashPassword(req.body.password);
+    console.log(req.body.password);
+
     const user = await addUser(req.body);
 
     user?._id
